c1935cccf339192b3d4ac5715e429445ce2e6e8d
Lee JaeBeom <woqja125@gmail.com>
2015-06-11 17:26:26 +0900

    Final
    Final

---
+++ Assets/Controller.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+
+	public GameObject SwingPrefab;
+	public Button btn;
+	public InputField S1P, S2P, S1G, S2G, Num;
+	public Toggle S1B, S1D, S1H, S2B, S2D, S2H;
+	public Text FrameRate, S1M, S2M;
+	GameObject swing1, swing2;
+	Swing S1, S2;
+
+	void Awake()
+	{
+		Application.targetFrameRate = 60;
+	}
+
+	private float accum = 0; // FPS accumulated over the interval
+	private int frames = 0; // Frames drawn over the interval
+	private float timeleft = 0.5f; // Left time for current interval
+
+	void Update()
+	{
+		timeleft -= Time.deltaTime;
+		accum += Time.timeScale / Time.deltaTime;
+		++frames;
+
+		// Interval ended - update GUI text and start new interval
+		if (timeleft <= 0.0)
+		{
+			// display two fractional digits (f2 format)
+			float fps = accum / frames;
+			FrameRate.text = string.Format("{0:F2}", fps);
+			
+			timeleft = 0.5f;
+			accum = 0.0F;
+			frames = 0;
+		}
+
+		if (S1 == null) S1M.text = "최대높이 : ";
+		else S1M.text = "최대높이 : " + S1.getMaxHeight();
+
+		if (S2 == null) S2M.text = "최대높이 : ";
+		else S2M.text = "최대높이 : " + S2.getMaxHeight();
+
+	}
+
+	public void ButtonClicked()
+	{
+		StartCoroutine(controll());
+	}
+
+	IEnumerator controll()
+	{
+		int t;
+		btn.enabled = false;
+
+		if (swing1 != null)
+		{
+			Destroy(swing1);
+			swing1 = null;
+			S1 = null;
+		}
+		if (swing2 != null)
+		{
+			Destroy(swing2);
+			swing2 = null;
+			S2 = null;
+		}
+
+		swing1 = Instantiate(SwingPrefab) as GameObject;
+		swing1.name = "swing1";
+		swing1.transform.localPosition = new Vector3(-10, 0, 5);
+		S1 = (swing1.GetComponent("Swing") as Swing);
+        float p1 = System.Convert.ToSingle(S1P.text);
+		if (S1B.isOn) t = 2; else if (S1D.isOn) t = 10; else t = 16;
+		uint g1 = System.Convert.ToUInt32(S1G.text, t);
+
+		swing2 = Instantiate(SwingPrefab) as GameObject;
+		swing2.name = "swing2";
+		swing2.transform.localPosition = new Vector3(-6, 0, 5);
+		S2 = (swing2.GetComponent("Swing") as Swing);
+        float p2 = System.Convert.ToSingle(S2P.text);
+		if (S2B.isOn) t = 2; else if (S2D.isOn) t = 10; else t = 16;
+		uint g2 = System.Convert.ToUInt32(S2G.text, t);
+
+		yield return new WaitForSeconds(1.5f);
+
+		int cnt = int.Parse(Num.text);
+		((S1.Human.GetComponent("HumanSwing")) as HumanSwing).StartPlayGPC(g1, p1, cnt);
+		((S2.Human.GetComponent("HumanSwing")) as HumanSwing).StartPlayGPC(g2, p2, cnt);
+		yield return new WaitForSeconds(2.0f + (p1>p2?p1:p2) * cnt);
+		btn.enabled = true;
+	}
+}
---
+++ Assets/HumanSwing/HumanSwing.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanSwing : HumanBehaviour
+{
+
+	// Use this for initialization
+	new void Start()
+	{
+		base.Start();
+		up();
+	}
+
+	public void StartPlayGPC(uint g, float p, int cnt)
+	{
+		period = p;
+		gene = g;
+		NUM_LOOP_CNT = cnt;
+		StartCoroutine(playWithGene());
+	}
+
+	public void StartPlayP(float p)
+	{
+		period = p;
+		gene = 0xF8000000u;
+		StartCoroutine(playWithGene());
+	}
+
+	public void StartPlay(uint Gene)
+	{
+		gene = Gene;
+		StartCoroutine(playWithGene());
+	}
+
+	const int NUM_GENE = 32;
+	int NUM_LOOP_CNT = 8;
+	uint gene = 0;
+
+	float period = 4.65f;
+
+	IEnumerator playWithGene()
+	{
+		for (int i = 1; i <= NUM_LOOP_CNT; i++ )
+		{
+			for(int j=0; j<NUM_GENE; j++)
+			{
+				if ( ((gene >> j)&1) == 1) up();
+				else down();
+				yield return new WaitForSeconds(period / NUM_GENE);
+			}
+		}
+	}
+	
+	void up()
+	{
+		Vector3 X = new Vector3(1, 0, 0);
+		RightLap.targetRotation = Quaternion.AngleAxis(-10, X);
+		LeftLap.targetRotation = Quaternion.AngleAxis(-10, X);
+		RightL.targetRotation = Quaternion.AngleAxis(-10, X);
+		LeftL.targetRotation = Quaternion.AngleAxis(-10, X);
+
+		RightElbow.targetRotation = Quaternion.AngleAxis(-10, X);
+		LeftElbow.targetRotation = Quaternion.AngleAxis(-10, X);
+		RightS.targetRotation = Quaternion.AngleAxis(-10, X);
+		LeftS.targetRotation = Quaternion.AngleAxis(-10, X);
+	}
+
+	void down()
+	{
+        Vector3 X = new Vector3(1, 0, 0);
+        RightLap.targetRotation = Quaternion.AngleAxis(180, X);
+		LeftLap.targetRotation = Quaternion.AngleAxis(180, X);
+		RightL.targetRotation = Quaternion.AngleAxis(180, X);
+		LeftL.targetRotation = Quaternion.AngleAxis(180, X);
+
+		RightElbow.targetRotation = Quaternion.AngleAxis(170, X);
+		LeftElbow.targetRotation = Quaternion.AngleAxis(170, X);
+
+		RightS.targetRotation = Quaternion.AngleAxis(170, X);
+		LeftS.targetRotation = Quaternion.AngleAxis(170, X);
+        //
+	}
+
+}
